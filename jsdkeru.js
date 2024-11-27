c559bda6a1d9d2457625b615221804c26cb2ab41
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-27 18:55:00 +0900

    Add files via upload

---
+++ jsdkeru.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/tlatms1@main/sms.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
